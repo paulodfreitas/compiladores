0cfceeabbdcf526b5262a645a13126b0916e18ff
vrod <victor.rodrigues@dcc.ufmg.br>
2014-11-17 10:59:47 -0200

    Cria tipo Node

--- parser/types.h
+++ parser/types.h
@@ -1,3 +1,6 @@
+#include <list>
+#include <iostream>
+
 typedef struct _constant_t {
     char val[100];
     char type[20];
@@ -6,3 +9,21 @@ typedef struct _constant_t {
 typedef struct _op_t {
     char type[100];
 } __op_t;
+
+namespace nd {
+	typedef enum {
+		PROGRAM,
+		PROC_BODY,
+		IDENTIFIER
+	} NodeType;
+
+	class Node {
+	public:
+	  NodeType type;
+		std::string literal_value;
+		std::list<Node*> children;
+		Node(NodeType t) {
+			type = t;
+		}
+	};
+};
